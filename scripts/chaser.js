f88139b22e56e3ecfd572ce943adcfc2a398ae09
Ollie-Perry <92376694+Ollie-Perry@users.noreply.github.com>
2021-11-09 13:17:47 +0000

    fixed canvas

--- scripts/chaser.js
+++ scripts/chaser.js
@@ -3,18 +3,30 @@ let y = 0
 let easing = 0.1
 
 function setup() {
-    createCanvas(displayWidth, displayHeight);
+    createCanvas(windowWidth, windowHeight);
     fill(200, 99);
-    noStroke()
+    windowResized();
+    noStroke();
     drawingContext.shadowBlur = 10;
     drawingContext.shadowColor = 'black';
 }
 
 function draw() {
-    clear()
+    clear();
     let targetX = mouseX;
 	x += (targetX - x) * easing;
     let targetY = mouseY;
 	y += (targetY - y) * easing;
-    ellipse(x, y, 30, 30)
+    ellipse(x, y, 30, 30);
 }
+
+function windowResized() {
+    const css = getComputedStyle(canvas.parentElement),
+          mw = float(css.marginLeft) + float(css.marginRight),
+          mh = float(css.marginTop)  + float(css.marginBottom),
+          ww = float(css.width)  || windowWidth,
+          wh = float(css.height) || windowHeight,
+          w = round(ww - mw), h = round(wh - mh);
+  
+    resizeCanvas(w, h, true);
+  }
